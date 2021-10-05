0d06b771390388802671c6ed7ab4634096c04611
jinkyuhan <gkswlsrb95@gmail.com>
2021-10-05 14:42:09 +0900

    feat: authentication logic added

---
+++ common_api_server/src/main/java/com/karrotmvp/umeapartment/v1/auth/KarrotAuthenticationToken.java
@@ -0,0 +1,39 @@
+package com.karrotmvp.umeapartment.v1.auth;
+
+import java.util.Arrays;
+
+import com.karrotmvp.umeapartment.v1.user.UserProfileDto;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public class KarrotAuthenticationToken extends AbstractAuthenticationToken {
+
+  private final String credentials;
+  private final UserProfileDto principal;
+
+  public KarrotAuthenticationToken(String bearerToken) {
+    super(null);
+    this.credentials = bearerToken;
+    this.principal = null;
+    this.setAuthenticated(false);
+  }
+
+  public KarrotAuthenticationToken(String bearerToken, UserProfileDto userProfile) {
+    super(Arrays.asList(new SimpleGrantedAuthority("ROLE_USER")));
+    this.credentials = bearerToken;
+    this.principal = userProfile;
+    this.setAuthenticated(true);
+  }
+
+  @Override
+  public String getCredentials() {
+    return this.credentials;
+  }
+
+  @Override
+  public UserProfileDto getPrincipal() {
+    return this.principal;
+  }
+  
+}
