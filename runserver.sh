1e286b310c379ac0cea971714fe720c581146989
jinkyuhan <gkswlsrb95@gmail.com>
2021-10-07 23:27:40 +0900

    fix: rename workflow & ignore environment var

--- runserver.sh
+++ runserver.sh
@@ -2,23 +2,21 @@
 
 help() {
   echo 
-  echo "Usage: ./runserver.sh -p [profile-name] [options]"
-  echo "  Arguments"
-  echo "    [profile-name]       The name of profile to run (ex. dev, staging, production)\n"
+  echo "Usage: ./runserver.sh [options]"
   echo "  Options:"
   echo "    -b                   Update runnning server with rebuilding"
   echo "    -m                   Run on M1 Architecture"
   echo "    -s [service-name]    Run only specific service. Select one among maria_rdb, common_api_server"
 }
 
-#ERROR CODE
+# ERROR CODE
 INVALID_ARGUMENT=3
 
 
-while getopts "bp:hms:" opt
+# resolve option
+while getopts "bhms:" opt
 do
 case $opt in
-  p) profile=$OPTARG ;;
   b) build=true ;;
   h) help && exit 0 ;;
   m) m1=true ;;
@@ -26,26 +24,13 @@ case $opt in
   esac
 done
 
-case $profile in
-  dev)
-    filename=docker-compose.dev.yaml ;;
-  staging)
-    filename=docker-compose.staging.yaml ;;
-  production)
-    filename=docker-compose.production.yaml ;;
-  *)
-    RED='\033[0;31m'
-    NC='\033[0m'
-    echo "${RED}ERROR: Invalid argument, you must set aleast one profile${NC}"
-    help
-    exit $INVALID_ARGUMENT
-    ;;
-  esac
-
+filename=docker-compose.dev.yaml
 
 command="docker-compose -f ${filename}"
 
-if [[ $profile = "dev" && $m1 ]] 
+
+# m1 architecture case
+if [ $m1 ] 
 then
   command="${command} -f docker-compose.dev-m1.yaml"
 fi
@@ -53,12 +38,18 @@ fi
 command="${command} up -d"
 
 
+# rebuild case
 if [ $build ]
 then
   command="${command} --build"
 fi
 
 
+# select service
+case $service in
+  rdb) service=maria_rdb ;;
+  server) service=common_api_server ;;
+esac
 
 if [ $service ]
 then
