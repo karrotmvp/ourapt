a59cdcdf33574fc17585c75c1860c9f16aa13c8a
jinkyuhan <gkswlsrb95@gmail.com>
2021-10-17 00:58:06 +0900

    feat: production 아닐때 Authentication열어주기 위한 MockOAuthProvider

--- common_api_server/src/main/java/com/karrotmvp/ourapt/v1/auth/springsecurity/KarrotOAuthProvider.java
+++ common_api_server/src/main/java/com/karrotmvp/ourapt/v1/auth/springsecurity/KarrotOAuthProvider.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.codec.CodecException;
 import org.springframework.core.env.Environment;
@@ -28,6 +29,7 @@ import java.util.Arrays;
 import java.util.Optional;
 
 @Component
+@Profile({"production"})
 @AllArgsConstructor
 public class KarrotOAuthProvider implements AuthenticationProvider {
     // Spring Security loads all the 'AuthenticationProvider Beans' when it initializes ProviderManager(AuthenticationManager)
@@ -44,11 +46,6 @@ public class KarrotOAuthProvider implements AuthenticationProvider {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     public KarrotUserProfileDto asyncGetUserProfileFromKarrot(String accessToken) {
-        if (!Arrays.asList(env.getActiveProfiles()).contains("production")) {
-            if (accessToken.equals("Beaerer ")) {
-            }
-            return new KarrotUserProfileDto("userIdExample", "userNicknameExample");
-        }
         logger.info("KARROT_API_CALLED");
         Mono<KarrotResponseBody<KarrotUserProfileDto>> userProfileMono = webClient.mutate()
                 .defaultHeader(HttpHeaders.AUTHORIZATION, accessToken)
---
+++ common_api_server/src/main/java/com/karrotmvp/ourapt/v1/auth/springsecurity/MockOAuthProvider.java
@@ -0,0 +1,26 @@
+package com.karrotmvp.ourapt.v1.auth.springsecurity;
+
+import com.karrotmvp.ourapt.v1.common.property.KarrotProperty;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@Profile({"!production"})
+public class MockOAuthProvider extends KarrotOAuthProvider {
+    // MockOAuthProvider is a Bean that is injected instead of KarrotOAuthProvider when the Profile is not production
+
+    public MockOAuthProvider(WebClient webClient, KarrotProperty karrotProperty, Environment env) {
+        super(webClient, karrotProperty, env);
+    }
+
+    @Override
+    public KarrotUserProfileDto asyncGetUserProfileFromKarrot(String accessToken) {
+        String mockSeed = accessToken.substring(7);
+        return new KarrotUserProfileDto(
+                mockSeed + "_mock_user_id",
+                mockSeed + "_mock_nickname"
+        );
+    }
+}
