a551fbd796618cc168512fc32da355216b3327f5
jinkyuhan <gkswlsrb95@gmail.com>
2021-11-03 11:15:18 +0900

    fix: base aparment data fixed

--- common_api_server/src/test/java/com/karrotmvp/ourapt/OuraptApplicationTests.java
+++ common_api_server/src/test/java/com/karrotmvp/ourapt/OuraptApplicationTests.java
@@ -3,10 +3,15 @@ package com.karrotmvp.ourapt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.UUID;
+
 @SpringBootTest
 class OuraptApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
     }
 }
