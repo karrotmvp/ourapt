469888fe8258ede52d3c4637be2dc284c936d326
jinkyuhan <gkswlsrb95@gmail.com>
2021-11-08 23:16:30 +0900

    feat: add log interceptor2

--- common_api_server/src/main/java/com/karrotmvp/ourapt/v1/log/LogInterceptor.java
+++ common_api_server/src/main/java/com/karrotmvp/ourapt/v1/log/LogInterceptor.java
@@ -1,5 +1,7 @@
 package com.karrotmvp.ourapt.v1.log;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -8,12 +10,15 @@ import javax.servlet.http.HttpServletResponse;
 
 @Component
 public class LogInterceptor implements HandlerInterceptor {
+
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
     String instanceId = request.getHeader("Instance-Id");
     String regionId = request.getHeader("Region-Id");
     String authHeader = request.getHeader("Authorization");
-    System.out.println("[InstanceId:" + instanceId + ", RegionId: " + regionId + ", AccessToken: " + authHeader + "]");
+    logger.info("[InstanceId:" + instanceId + ", RegionId: " + regionId + ", AccessToken: " + authHeader + "]");
     return true;
   }
 }
