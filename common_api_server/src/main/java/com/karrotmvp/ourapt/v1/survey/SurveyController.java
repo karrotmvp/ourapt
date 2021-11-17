44ad215b47bf30de69f7aaaffe5f3aede1a54a2a
jinkyuhan <gkswlsrb95@gmail.com>
2021-11-17 16:49:00 +0900

    feat: 임시 설문용 컨트롤러

---
+++ common_api_server/src/main/java/com/karrotmvp/ourapt/v1/survey/SurveyController.java
@@ -0,0 +1,41 @@
+package com.karrotmvp.ourapt.v1.survey;
+
+import com.karrotmvp.ourapt.v1.auth.CurrentUser;
+import com.karrotmvp.ourapt.v1.common.CommonResponseBody;
+import com.karrotmvp.ourapt.v1.user.entity.KarrotProfile;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
+
+@RestController
+@RequestMapping(value = "/v1/api/survey")
+@Api(tags = "설문용 임시 controller")
+public class SurveyController {
+
+  @Autowired
+  private EntityManager em;
+
+  @PostMapping("")
+  @Transactional
+  @ApiOperation("[임시] 설문조사 제출")
+  public CommonResponseBody<Void> submitSurvey(
+    @RequestBody SurveyDto form,
+    @CurrentUser KarrotProfile profile
+  ) {
+    Survey s = new Survey();
+    s.setUserId(profile.getId());
+    s.setContentJson(form.getContentJson());
+    em.persist(s);
+    return CommonResponseBody.<Void>builder()
+      .success()
+      .build();
+  }
+
+}
---
+++ common_api_server/src/main/java/com/karrotmvp/ourapt/v1/survey/SurveyDto.java
@@ -0,0 +1,12 @@
+package com.karrotmvp.ourapt.v1.survey;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class SurveyDto {
+  private String contentJson;
+}
